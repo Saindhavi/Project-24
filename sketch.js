4280f0ccf7861ca65aa9a30f9116f3a232de5582
Saindhavi <72200594+Saindhavi@users.noreply.github.com>
2020-12-04 16:06:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+  createCanvas(1200, 700);
+  
+    engine = Engine.create();
+	  world = engine.world;
+
+	//Create the Bodies Here.
+
+    paper1 = new Paper(500,300);
+    ground = new Ground(600,700,1200,20);
+
+    boxPosition=width/2-100
+    boxY=610;
+ 
+ 
+    boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+    boxleftSprite.shapeColor=color(255,0,0);
+ 
+    boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+    World.add(world, boxLeftBody);
+ 
+    boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+    boxBase.shapeColor=color(255,0,0);
+ 
+    boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+    World.add(world, boxBottomBody);
+ 
+    boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+    boxleftSprite.shapeColor=color(255,0,0);
+ 
+    boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+    World.add(world, boxRightBody);
+
+}
+
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+
+    Matter.Body.applyForce(paperObject.Body,paperObject.Body.Position,{x:85,y:-85});
+
+  }
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.run(engine);
+
+  paper1.display();
+  ground.display();
+
+  drawSprites();
+ 
+}
+
+
+
